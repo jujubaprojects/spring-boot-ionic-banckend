e1896978fa985663afefaf1ea4d8eaf74ad9c947
Julio Ferreira <julioengdc@gmail.com>
2019-11-09 15:12:05 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/example/cursomc/demo/DemoApplication.java
+++ src/main/java/com/example/cursomc/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.cursomc.demo;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.example.cursomc.demo.domain.Cidade;
 import com.example.cursomc.demo.domain.Cliente;
 import com.example.cursomc.demo.domain.Endereco;
 import com.example.cursomc.demo.domain.Estado;
+import com.example.cursomc.demo.domain.Pagamento;
+import com.example.cursomc.demo.domain.PagamentoComBoleto;
+import com.example.cursomc.demo.domain.PagamentoComCartao;
+import com.example.cursomc.demo.domain.Pedido;
 import com.example.cursomc.demo.domain.Produto;
+import com.example.cursomc.demo.domain.enums.EstadoPagamento;
 import com.example.cursomc.demo.domain.enums.TipoCliente;
 import com.example.cursomc.demo.repositories.CategoriaRepository;
 import com.example.cursomc.demo.repositories.CidadeRepository;
 import com.example.cursomc.demo.repositories.ClienteRepository;
 import com.example.cursomc.demo.repositories.EnderecoRepository;
 import com.example.cursomc.demo.repositories.EstadoRepository;
+import com.example.cursomc.demo.repositories.PagamentoRepository;
+import com.example.cursomc.demo.repositories.PedidoRepository;
 import com.example.cursomc.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -38,6 +46,11 @@ public class DemoApplication implements CommandLineRunner
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	public static void main(String[] args) 
 	{
@@ -87,6 +100,21 @@ public class DemoApplication implements CommandLineRunner
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 	
 	
--- src/main/java/com/example/cursomc/demo/domain/Cliente.java
+++ src/main/java/com/example/cursomc/demo/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.example.cursomc.demo.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable
@@ -32,12 +33,16 @@ public class Cliente implements Serializable
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente()
 	{
@@ -126,5 +131,11 @@ public class Cliente implements Serializable
 	}
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 }
