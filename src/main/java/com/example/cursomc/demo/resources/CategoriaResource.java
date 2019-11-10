adddc8871a078242bbdd6aff6c94389296d24222
Julio Ferreira <julioengdc@gmail.com>
2019-11-10 15:00:50 -0300

    Listando Categorias com DTO

--- src/main/java/com/example/cursomc/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/demo/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.example.cursomc.demo.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.cursomc.demo.domain.Categoria;
+import com.example.cursomc.demo.dto.CategoriaDTO;
 import com.example.cursomc.demo.services.CategoriaService;
 
 @RestController
@@ -34,6 +37,14 @@ public class CategoriaResource
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll()
+	{
+		List<Categoria> lista = service.findAll();		
+		List<CategoriaDTO> listaDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id)
--- src/main/java/com/example/cursomc/demo/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/demo/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.cursomc.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,17 @@ public class CategoriaService
 {
 	@Autowired
 	private CategoriaRepository repo;
-		
+	
+	public List<Categoria> findAll ()
+	{
+		return repo.findAll();
+	}
+
 	public Categoria find (Integer id)
 	{
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	Optional<Categoria> obj = repo.findById(id);
+	return obj.orElseThrow(() -> new ObjectNotFoundException(
+			"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	public Categoria insert(Categoria obj)
