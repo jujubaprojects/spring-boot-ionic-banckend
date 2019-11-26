f47c835f149ae32b93c41473aba06d5a31ac59c8
Julio Ferreira <julioengdc@gmail.com>
2019-11-26 20:37:41 -0300

    Configuracao Inicial do Spring Security

---
+++ src/main/java/com/example/cursomc/demo/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.cursomc.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+	private static final String[] PUBLIC_MATCHERS = 
+	{
+			"/h2-console/**",
+	};
+	private static final String[] PUBLIC_MATCHERS_GET = 
+	{
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	
+	@Autowired
+	private Environment env;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		if (Arrays.asList(env.getActiveProfiles()).contains("teste"))
+			http.headers().frameOptions().disable();
+			
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	/*@Bean
+	CorsConfigurationSource corsConfigurationSource()
+	{
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return (CorsConfigurationSource) source;
+	}*/
+}
