ff6123e9539ed74d0bdb118305ca9f3f4c53e310
Julio Ferreira <julioengdc@gmail.com>
2019-11-26 21:42:23 -0300

    Adicionando senha ao cliente

--- src/main/java/com/example/cursomc/demo/services/DBService.java
+++ src/main/java/com/example/cursomc/demo/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.cursomc.demo.domain.Categoria;
@@ -56,6 +57,9 @@ public class DBService
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	
 	
 	public void instantiateDatabase() throws ParseException
@@ -119,7 +123,7 @@ public class DBService
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "xjujuba206x@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "xjujuba206x@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 
 		Endereco e1 = new Endereco(null,"Rua Flores", "300", "Apto 203", "Jardim", "38220834",cli1, c1);
